71f61ff2c5fef4b23190bc1dd3f56aba1358b136
Sergio <serggio.rs@gmail.com>
2025-12-02 14:39:44 -0300

    add redirect resetpasswd

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/api', 302);
+Route::get('/reset-password/{token}', function ($token) {
+    return redirect("https://porfolio.esencialtic.com.ar/reset-password?token={$token}&email=" . request('email'));
+})->name('password.reset');
